7e420c91e9bf8e3a749e09240bc85fff770d0b86
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-01-11 19:18:31 +0200

    lab12

--- app.js
+++ app.js
@@ -15,10 +15,29 @@ var orderSection = document.getElementById('all_orders');
 
 var listitem = document.getElementById('list');
 
+var chartItem = document.getElementById('chart');
+
+var chartOrder =document.getElementById('orderPickChart').getContext('2d');
+
 var trialsleft= 25;
 
+var arrayOfclicks =[];
 
+var arrayOfshowImged =[];
 
+var arrayOfnames=['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog duck','dragon','pen','pet sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water can','wine glass']
+
+var arrayOfpics =['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg']
+
+var arrayOfcolors= ["#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
+"#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
+"#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e"]
+
+for (var index = 0; index < arrayOfpics.length; index++) {
+   
+  new Order (arrayOfnames[index],arrayOfpics[index]);
+  
+}  
 
 function Order(name,image){
     this.name=name;
@@ -28,6 +47,7 @@ function Order(name,image){
     this.counterShowImages=0;
 
     arrayOforders.push(this);
+
 }
 
 
@@ -39,26 +59,43 @@ function renderOrder(leftImg,middleImg,rightImg){
     arrayOforders[rightImg].counterShowImages++;
     arrayOforders[middleImg].counterShowImages++;
     arrayOforders[leftImg].counterShowImages++;
-   
+    
+    
 
     leftOrderText.textContent = arrayOforders[leftImg].name;
     middleOrderText.textContent = arrayOforders[middleImg].name;
     rightOrderText.textContent = arrayOforders[rightImg].name;
 }
 
-
+function checkAvailability (selectedImageName){
+  for (var index = 0; index < shownImgAlready.length; index++) {
+    if (shownImgAlready[index].name===selectedImageName) {
+      return true;
+    }
+    
+  }
+  return false;
+}
+var shownImgAlready=[];
 
 function pickAnOrder(){
     var leftImg = Math.round(Math.random()*(arrayOforders.length-1));
-
-
+    var leftImgName= arrayOforders[leftImg].name;
+    checkAvailability(leftImgName);
+    
     do{
         var middleImg =  Math.round(Math.random()*(arrayOforders.length-1));
         var rightImg= Math.round(Math.random()*(arrayOforders.length-1));
-    }while(leftImg===rightImg || middleImg===rightImg||leftImg===middleImg);
+    }while(leftImg===rightImg || middleImg===rightImg||leftImg===middleImg|| checkAvailability(arrayOforders[rightImg].name)||checkAvailability(arrayOforders[middleImg].name));
 
+
+    shownImgAlready=[];
+
+    shownImgAlready.push(
+      arrayOforders[leftImg],arrayOforders[middleImg],arrayOforders[rightImg]
+    )
     renderOrder(leftImg,middleImg,rightImg);
-    
+  
 }
 
 
@@ -83,28 +120,28 @@ function countOrders(event){
             var objectIndicator = event.target.getAttribute('src');
             checkOrders(objectIndicator);
             pickAnOrder();
+
         }
     }else{
-        orderSection.removeEventListener('click',countOrders)
+        orderSection.removeEventListener('click',countOrders);
+        for (var index = 0; index < arrayOfnames.length; index++) {
+          arrayOfshowImged.push(arrayOforders[index].counterShowImages); 
+          arrayOfclicks.push(arrayOforders[index].counterClick++);
+        }
+        console.log(arrayOfshowImged);
+        console.log(arrayOfclicks);
+        chartItem.addEventListener('click' ,chartForOrders);
+
+        
     }
 }
 
 
 
-var arrayOfnames=['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog duck','dragon','pen','pet sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water can','wine glass']
-
-var arrayOfpics =['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg']
-
-for (var index = 0; index < arrayOfpics.length; index++) {
-   
-    new Order (arrayOfnames[index],arrayOfpics[index]);
-    
-}
-
-
 pickAnOrder();
 orderSection.addEventListener('click', countOrders);
 listitem.addEventListener('click', fillList);
+
 
 function fillList(){
 
@@ -116,3 +153,38 @@ function fillList(){
     }
 }
 
+function chartForOrders (){
+
+  var chartOrder =document.getElementById('orderPickChart').getContext('2d');
+
+  var myChart = new Chart(chartOrder, {
+    type: 'bar',
+    data: {
+        labels: arrayOfnames,
+        datasets: [{
+            label: '# show Image',
+            data: arrayOfshowImged,
+            backgroundColor:arrayOfcolors,
+            borderColor: arrayOfcolors,
+            borderWidth: 1
+        },
+        {
+          label: '# clicks',
+          data: arrayOfclicks,
+          backgroundColor: arrayOfcolors,
+          borderColor: arrayOfcolors,
+          borderWidth: 1
+      }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+}
